6755fb9c76ac610695153dbb3da86022a651a9aa
doormoonn <robort123@gachon.ac.kr>
2025-08-12 18:36:13 +0900

    real log

---
+++ src/test/java/com/example/hanaro/repository/CartRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.example.hanaro.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.hanaro.RepositoryTest;
+
+class CartRepositoryTest extends RepositoryTest {
+
+}
